c3193d794433472acf93979d13635c42a8d448b1
TauanAnjos <tauandosanjosnascimento@gmail.com>
2024-04-12 13:42:03 -0300

    CRUD disciplina

--- api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/service/DisciplinaService.java
+++ api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/service/DisciplinaService.java
@@ -1,11 +1,31 @@
 package br.edu.ifs.apinewsigaa.service;
 
+import br.edu.ifs.apinewsigaa.model.DisciplinaModel;
 import br.edu.ifs.apinewsigaa.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DisciplinaService {
     @Autowired
     private DisciplinaRepository disciplinaRepository;
+
+    public DisciplinaModel saveDisciplina(DisciplinaModel disciplinaModel) {
+        return disciplinaRepository.save(disciplinaModel);
+    }
+
+    public List<DisciplinaModel> getAllDisciplinas() {
+        return disciplinaRepository.findAll();
+    }
+
+    public Optional<DisciplinaModel> getDisciplinaId(int disciplinaId) {
+        return disciplinaRepository.findById(disciplinaId);
+    }
+
+    public void deleteDisciplina(int disciplinaId) {
+        disciplinaRepository.deleteById(disciplinaId);
+    }
 }
