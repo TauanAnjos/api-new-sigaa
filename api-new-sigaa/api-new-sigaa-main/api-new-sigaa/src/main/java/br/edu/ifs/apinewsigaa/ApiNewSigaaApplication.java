6973aaf3e3cfc5f1e4809acbbc416ed8af3f868a
Tauan Anjos <tauandosanjosnascimento@gmail.com>
2024-09-12 10:13:16 -0300

    implementação de autenticação com keycloak

--- api-new-sigaa/api-new-sigaa-main/api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/ApiNewSigaaApplication.java
+++ api-new-sigaa/api-new-sigaa-main/api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/ApiNewSigaaApplication.java
@@ -1,9 +1,25 @@
 package br.edu.ifs.apinewsigaa;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * Define o esquema de segurança para integração com o Keycloak.
+ * O esquema é configurado como OPENIDCONNECT com tipo de autenticação "bearer".
+ * A URL de conexão é fornecida para o endpoint OpenID Connect do Keycloak.
+ * Os tokens de autenticação são passados no cabeçalho da solicitação (HEADER).
+ */
+@SecurityScheme(
+		name = "Keycloak"
+		, openIdConnectUrl = "http://localhost:8081/realms/newsigaa/.well-known/openid-configuration"
+		, scheme = "bearer"
+		, type = SecuritySchemeType.OPENIDCONNECT
+		, in = SecuritySchemeIn.HEADER
+)
 public class ApiNewSigaaApplication {
 
 	public static void main(String[] args) {
